0c9e922ec0f4cdc2ff4739e0c7fa3f2c2279ec4b
piyushhingu <hingupiyush3@gmail.com>
2025-01-09 11:38:33 +0530

    this is cmake file

--- Kalido-comp/build/cmake_install.cmake
+++ Kalido-comp/build/cmake_install.cmake
@@ -39,7 +39,7 @@ endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+  set(CMAKE_OBJDUMP "/usr/local/bin/llvm-objdump")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
